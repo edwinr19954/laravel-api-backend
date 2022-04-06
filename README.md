a069d37a60f2398eedb6b417d1148e6ec278bbc2
Edwin Rivera <edwinr19954@hotmail.com>
2022-04-06 18:04:00 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1 align="center">Laravel BackEnd API</h1>
+
+# How To Install
+* `composer Install`
+* Create .env file in the project root directory
+* Configure Database Details
+```
+DB_CONNECTION=mysql 
+DB_HOST=127.0.0.1 
+DB_PORT=3306 
+DB_DATABASE=<DATABASE NAME>
+DB_USERNAME=<DATABASE USERNAME>
+DB_PASSWORD=<DATABASE PASSWORD>
+```
+* `php artisan migrate`
+* `php artisan storage:link`
+* `php artisan serve`
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Attempt User Login.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+ 
+            return response()->json([
+                'message'=> 'Information is Correct!!',
+                'data' => $request->user()
+            ]);
+        }
+ 
+        return response()->json([
+            'message'=>'Information is InCorrect!!',
+        ],500);
+
+    }
+
+    /**
+     * Attempt User Login.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return response()->json([
+            'message'=> 'LogOut',
+        ]);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+        
+        try{
+            User::create(array_merge($request->post(),[
+                'remember_token' => \Hash::make($request->password),
+                'password' => \Hash::make($request->password)
+            ]));
+
+            return response()->json([
+                'message'=>'Account Created Successfully!!'                
+            ]);
+        }catch(\Exception $e){
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message'=>'Something goes wrong while creating an account!!',
+            ],500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
